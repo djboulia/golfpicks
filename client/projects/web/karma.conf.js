bf35da03a22550d3880c1c2257ed70a5adf8c79a
Don Boulia <djboulia@gmail.com>
2022-11-22 09:36:34 -0500

    move to multi-project angular structure before add of mobile

--- client/projects/web/karma.conf.js
+++ client/projects/web/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/golfpicks-client'),
+      dir: require('path').join(__dirname, '../../coverage/web'),
       subdir: '.',
       reporters: [
         { type: 'html' },
