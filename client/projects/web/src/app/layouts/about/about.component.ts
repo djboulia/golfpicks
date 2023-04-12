f6dbd449118676b6f1444d67f29407e94022b80b
djboulia <djboulia@gmail.com>
2023-04-12 16:24:44 -0400

    add scss file for about component

--- client/projects/web/src/app/layouts/about/about.component.ts
+++ client/projects/web/src/app/layouts/about/about.component.ts
@@ -1,9 +1,8 @@
-import { Component, } from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
-  templateUrl: './about.component.html'
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
-  constructor() {
-  }
-
+  constructor() {}
 }
