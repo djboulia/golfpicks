0eb8d76bfea0ed06ad26aa93904625599c9a6f2e
Don Boulia <djboulia@gmail.com>
2025-04-12 10:14:49 -0400

    account for CUT playeres

--- client/projects/web/src/app/component/leaderboard-components/user-score-board/user-score-board.component.ts
+++ client/projects/web/src/app/component/leaderboard-components/user-score-board/user-score-board.component.ts
@@ -18,14 +18,26 @@ export class UserScoreBoardComponent implements OnInit {
   ngOnInit(): void {
     this.scores = [...this.gamer.scores];
 
-    for (let i = 0; i < this.scores.length; i++) {
-      if (this.scores[i] === '-') {
-        this.scores[i] = '';
-      } else {
-        if (i < this.scores.length - 1) {
-          if (this.scores[i + 1] !== '-') {
-            // later score is available, remove earlier scores
-            this.scores[i] = '';
+    const cutIndex = this.scores.indexOf('CUT');
+    if (cutIndex > -1) {
+      // remove all scores except the first CUT
+      this.scores = this.scores.map((_score: any, index: number) => {
+        if (index === cutIndex) {
+          return 'CUT';
+        }
+        return '';
+      });
+    } else {
+      // player not cut, now just show the current active score
+      for (let i = 0; i < this.scores.length; i++) {
+        if (this.scores[i] === '-') {
+          this.scores[i] = '';
+        } else {
+          if (i < this.scores.length - 1) {
+            if (this.scores[i + 1] !== '-') {
+              // later score is available, remove earlier scores
+              this.scores[i] = '';
+            }
           }
         }
       }
