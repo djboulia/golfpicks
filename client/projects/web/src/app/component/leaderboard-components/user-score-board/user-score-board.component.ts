1ff8164574309cb3930eb532446fa4db557e9b3e
Don Boulia <djboulia@gmail.com>
2025-04-12 09:45:26 -0400

    change format of individual gamer score cards

--- client/projects/web/src/app/component/leaderboard-components/user-score-board/user-score-board.component.ts
+++ client/projects/web/src/app/component/leaderboard-components/user-score-board/user-score-board.component.ts
@@ -9,7 +9,29 @@ export class UserScoreBoardComponent implements OnInit {
   @Input() gamer: any;
   @Input() roundTitles: string[] = [];
 
+  top5: any = [];
+  bottom5: any = [];
+  scores: any = [];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.scores = [...this.gamer.scores];
+
+    for (let i = 0; i < this.scores.length; i++) {
+      if (this.scores[i] === '-') {
+        this.scores[i] = '';
+      } else {
+        if (i < this.scores.length - 1) {
+          if (this.scores[i + 1] !== '-') {
+            // later score is available, remove earlier scores
+            this.scores[i] = '';
+          }
+        }
+      }
+    }
+
+    this.top5 = this.gamer.picks.slice(0, 5);
+    this.bottom5 = this.gamer.picks.slice(-5);
+  }
 }
